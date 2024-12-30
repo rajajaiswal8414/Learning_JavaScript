75b693027da7d9197b4d6fd7fea17dd3e61d28f9
Raja Jaiswal <143202200+rajajaiswal8414@users.noreply.github.com>
2024-12-30 19:12:00 +0000

    commit

---
+++ 01_Basics/05_datatypes.js
@@ -0,0 +1,20 @@
+// Primitive
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId); => false
+
+const BigInt = 1234567891011121314n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
