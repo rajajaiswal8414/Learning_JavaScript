8dc556d5c26c53f1945487fd63a6a7ccc7cac9fb
Raja Jaiswal <143202200+rajajaiswal8414@users.noreply.github.com>
2024-12-30 19:10:21 +0000

    commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,75 @@
+let score = "Raja"
+let score1 = null
+let score2 = undefined
+// console.log(typeof score); => string
+// console.log(typeof(score)); => string
+
+// console.log(typeof score1) => Object
+
+// let valueInNumber = Number(score) -> converts to number
+// console.log(typeof valueInNumber); -> output: number
+// console.log(valueInNumber) -> output: NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => typeof -> undefined
+// null => typeof -> Object
+// null => typeof Number(null) => number
+// null => console.log(Number(null)) => 0
+// console.log(Number(null)); => 0
+// console.log(null) = null
+// console.log(Number(undefined)) => NaN
+// console.log(typeof Number(undefined)) => undefined
+
+let isLoggedIn = "Raja"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); -> true
+
+// "" => false
+// "Raja" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber); => 33
+// console.log(typeof stringNumber) => string
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue) => -3
+
+// console.log(2+2); => 4
+// console.log(2-2); => 0
+// console.log(2*2); => 4
+// console.log(2**3); => 8
+// console.log(2/3); => 0.6666666666666666
+// console.log(2%3); => 2
+
+let str1 = "Hello "
+let str2 = "Raja"
+
+let str3 = str1 + str2
+// console.log(str3); => concat two string "Hello Raja"
+
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); = > 12
+// console.log("1" + 2 + 2); => 122
+
+// console.log(1 + true); => 2
+// console.log(+true) => 1
+// console.log(true) => true
+
+// console.log(+""); => 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter) => 101
+
+
+
