cad787526edb368406c6c6c782db1cc85299c04e
Raja Jaiswal <143202200+rajajaiswal8414@users.noreply.github.com>
2024-12-31 18:55:13 +0000

    changes

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -79,3 +79,11 @@ console.log(instructor);
     {},
     {}
 ]
+
+// const target = { a: 1, b: 2 };
+// const source = { b: 4, c: 5 };
+
+// const returnedTarget = Object.assign(target, source);
+// console.log(target);
+// console.log(returnedTarget === target);
+
